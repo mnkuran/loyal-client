9b3fe53cdb23b7581a91ea9685d838e41247126b
Mahmut Kuran <mahmutkuran@hotmail.com>
2020-09-16 17:54:18 +0300

    Add Firebase sdk and init firebase app

--- src/main.js
+++ src/main.js
@@ -1,9 +1,24 @@
 import Vue from "vue";
+
+import firebase from "firebase/app";
+
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
 Vue.config.productionTip = false;
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDyQfcwJW6I06tif06sEsrq-TAUxW8--9c",
+  authDomain: "kuran-loyal.firebaseapp.com",
+  databaseURL: "https://kuran-loyal.firebaseio.com",
+  projectId: "kuran-loyal",
+  storageBucket: "kuran-loyal.appspot.com",
+  messagingSenderId: "1076892292545",
+  appId: "1:1076892292545:web:645ffd66f4902154914b40"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
