13a78a614a3225455f4ebd6db0cd364e4a7fc154
Mahmut Kuran <mahmutkuran@hotmail.com>
2020-09-17 09:36:20 +0300

    Use vuefire for fetching customers

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import Vue from "vue";
 
+import { firestorePlugin } from "vuefire";
+
 import { auth } from "./firebase";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
 Vue.config.productionTip = false;
+Vue.use(firestorePlugin);
 
 let app;
 auth.onAuthStateChanged((user) => {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,26 +3,45 @@
     <h2>Home</h2>
     <login-display />
     <button @click="logout">Logout</button>
+    <div v-for="customer in customers" :key="customer.id">
+      <h3>{{ customer.name }}</h3>
+      <p>{{ customer.id }}</p>
+      <p>
+        <strong>Points: </strong><span>{{ customer.points }}</span>
+      </p>
+      <hr />
+    </div>
   </div>
 </template>
 
 <script>
-import { auth } from "../firebase";
+import { auth, db } from "../firebase";
 import LoginDisplay from "../components/LoginDisplay";
 
 export default {
+  data() {
+    return {
+      customers: [],
+    };
+  },
   components: {
-    LoginDisplay
+    LoginDisplay,
   },
   methods: {
     async logout() {
       try {
         await auth.signOut();
         this.$router.push({ name: "Login" });
-      } catch(error) {
+      } catch (error) {
         alert(error);
       }
-    }
-  }
-}
+    },
+  },
+  firestore: {
+    customers: db
+      .collection("users")
+      .doc("lUw28EDnFUOscJSlttpDC8SH5Yy1")
+      .collection("customers"),
+  },
+};
 </script>
